b8d413b6b6a73e51db8b5816a1dcf691affcf8a4
stevium <milos@mstevanovic.net>
2023-10-15 00:42:46 +0200

    Add chrome newtab support

--- pages/blank.js
+++ pages/blank.js
@@ -1,4 +1,8 @@
+if (location.search !== "?x") {
+    location.search = "?x";
+    throw new Error;  // load everything on the next page;
+}
 Vomnibar.init();
 setTimeout(() => {
-    Vomnibar.vomnibarUI.activate({name:"activate", selectFirst: true});
+    Vomnibar.vomnibarUI.activate({name:"activate", selectFirst: true, focus: true});
 }, 10);
